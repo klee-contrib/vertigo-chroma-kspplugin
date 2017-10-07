d469c37f15982d4dadfac150f7162f18134bc344
sebez <sebez.boulot@gmail.com>
2017-10-07 13:59:08 +0200

    Relecture Sonar.

--- vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/KspStringUtils.java
+++ vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/KspStringUtils.java
@@ -127,28 +127,6 @@ public final class KspStringUtils {
 		return getKspAttribute(lineContent, KASPER_6_KSP_ATTRIBUTE);
 	}
 
-	private static KspAttribute getKspAttribute(String lineContent, Pattern attributePattern) {
-		/* Premier pattern pour détecter l'attribut. */
-		Matcher matcherAttribute = attributePattern.matcher(lineContent);
-		if (matcherAttribute.matches()) {
-			String attributeName = matcherAttribute.group(1);
-			KspAttribute attribute = new KspAttribute(attributeName);
-			String allProperties = matcherAttribute.group(2);
-
-			/* Second pattern pour détecter les paires clé-valeur de propriétés. */
-			Matcher matcherProperties = VERTIGO_KSP_ATTRIBUTE_PROPERTIES.matcher(allProperties);
-			while (matcherProperties.find()) {
-				String propertyName = matcherProperties.group(1);
-				String propertyValue = matcherProperties.group(2);
-				attribute.addProperty(propertyName, propertyValue);
-			}
-
-			return attribute;
-		}
-
-		return null;
-	}
-
 	public static KspAttribute getKasper5KspAttribute(String lineContent) {
 
 		/* Premier pattern pour détecter l'attribut. */
@@ -292,6 +270,28 @@ public final class KspStringUtils {
 		if (matcher.matches()) {
 			return matcher.group(1);
 		}
+		return null;
+	}
+
+	private static KspAttribute getKspAttribute(String lineContent, Pattern attributePattern) {
+		/* Premier pattern pour détecter l'attribut. */
+		Matcher matcherAttribute = attributePattern.matcher(lineContent);
+		if (matcherAttribute.matches()) {
+			String attributeName = matcherAttribute.group(1);
+			KspAttribute attribute = new KspAttribute(attributeName);
+			String allProperties = matcherAttribute.group(2);
+	
+			/* Second pattern pour détecter les paires clé-valeur de propriétés. */
+			Matcher matcherProperties = VERTIGO_KSP_ATTRIBUTE_PROPERTIES.matcher(allProperties);
+			while (matcherProperties.find()) {
+				String propertyName = matcherProperties.group(1);
+				String propertyValue = matcherProperties.group(2);
+				attribute.addProperty(propertyName, propertyValue);
+			}
+	
+			return attribute;
+		}
+	
 		return null;
 	}
 }
