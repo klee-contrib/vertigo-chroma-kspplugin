1cb63af078c47a456ced2c3c9670928e84dacb4e
sebez <sebez.boulot@gmail.com>
2017-11-30 07:03:21 +0100

    Sauvegarde de la version Kasper/Vertigo d'un projet dans le .settings du projet. Fix #9.

--- vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/PropertyUtils.java
+++ vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/PropertyUtils.java
@@ -3,8 +3,14 @@ package io.vertigo.chroma.kspplugin.utils;
 import io.vertigo.chroma.kspplugin.Activator;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * Méthodes utilitaires pour gérer les propriétés d'une ressource.
@@ -12,33 +18,52 @@ import org.eclipse.core.runtime.QualifiedName;
 public final class PropertyUtils {
 
 	private static final String LEGACY_VERSION_PROPERTY = "LEGACY_VERSION";
-	private static final QualifiedName LEGACY_VERSION_PROPERTY_KEY = new QualifiedName(Activator.PLUGIN_ID, LEGACY_VERSION_PROPERTY);
 
 	private PropertyUtils() {
 		// RAS.
 	}
 
 	public static String getLegacyVersion(IProject project) {
-		try {
-			String legacyVersionName = project.getPersistentProperty(LEGACY_VERSION_PROPERTY_KEY);
-			if (legacyVersionName == null || legacyVersionName.isEmpty()) {
-				return null;
-			}
-
-			return legacyVersionName;
-
-		} catch (CoreException e) {
-			ErrorUtils.handle(e);
+		IScopeContext projectScope = new ProjectScope(project);
+		IEclipsePreferences node = projectScope.getNode(Activator.PLUGIN_ID);
+		if (node == null) {
+			LogUtils.info("Get legacy version : node null !");
+			return null;
+		}
+		String legacyVersionName = node.get(LEGACY_VERSION_PROPERTY, null);
+		if (legacyVersionName == null || legacyVersionName.isEmpty()) {
+			return null;
 		}
 
-		return null;
+		return legacyVersionName;
 	}
 
 	public static void setLegacyVersion(IProject project, String legacyVersionName) {
-		try {
-			project.setPersistentProperty(LEGACY_VERSION_PROPERTY_KEY, legacyVersionName);
-		} catch (CoreException e) {
-			ErrorUtils.handle(e);
-		}
+		/*
+		 * On fait l'action dans un job asynchrone pour éviter une ResourceException: The resource tree is locked for modifications
+		 */
+
+		Job job = new Job("KspPreferenceSaving") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				IScopeContext projectScope = new ProjectScope(project);
+				IEclipsePreferences node = projectScope.getNode(Activator.PLUGIN_ID);
+				if (node == null) {
+					LogUtils.info("Set legacy version : node null !");
+					return Status.OK_STATUS;
+				}
+				node.put(LEGACY_VERSION_PROPERTY, legacyVersionName);
+				try {
+					node.flush();
+				} catch (BackingStoreException e) {
+					ErrorUtils.handle(e);
+				}
+				return Status.OK_STATUS;
+			}
+		};
+
+		job.setPriority(Job.SHORT);
+		job.schedule();
 	}
 }
