777346b4d07231aa870e0a5ed7f553ec6333005a
sebez <sebez.boulot@gmail.com>
2017-12-02 17:58:49 +0100

    Refactoring de PropertyUtils avant ajout d'une nouvelle propriété.

--- vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/PropertyUtils.java
+++ vertigo-chroma-plugin/src/io/vertigo/chroma/kspplugin/utils/PropertyUtils.java
@@ -17,53 +17,78 @@ import org.osgi.service.prefs.BackingStoreException;
  */
 public final class PropertyUtils {
 
-	private static final String LEGACY_VERSION_PROPERTY = "LEGACY_VERSION";
+	private static final ProjectPropertyDescriptor LEGACY_VERSION = new ProjectPropertyDescriptor("LEGACY_VERSION");
 
 	private PropertyUtils() {
 		// RAS.
 	}
 
 	public static String getLegacyVersion(IProject project) {
-		IScopeContext projectScope = new ProjectScope(project);
-		IEclipsePreferences node = projectScope.getNode(Activator.PLUGIN_ID);
-		if (node == null) {
-			LogUtils.info("Get legacy version : node null !");
-			return null;
-		}
-		String legacyVersionName = node.get(LEGACY_VERSION_PROPERTY, null);
-		if (legacyVersionName == null || legacyVersionName.isEmpty()) {
-			return null;
-		}
-
-		return legacyVersionName;
+		return LEGACY_VERSION.getValue(project);
 	}
 
 	public static void setLegacyVersion(IProject project, String legacyVersionName) {
-		/*
-		 * On fait l'action dans un job asynchrone pour éviter une ResourceException: The resource tree is locked for modifications
-		 */
+		LEGACY_VERSION.setValue(project, legacyVersionName);
+	}
 
-		Job job = new Job("KspPreferenceSaving") {
+	/**
+	 * Descripteur de propriété d'un projet permettant d'obtenir et de définir la valeur de la propriété.
+	 * <p>
+	 * La valeur est stockée dans un fichier propre au plugin, dans le dossier .settings du projet.
+	 * </p>
+	 */
+	private static class ProjectPropertyDescriptor {
+		private final String propertyName;
 
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				IScopeContext projectScope = new ProjectScope(project);
-				IEclipsePreferences node = projectScope.getNode(Activator.PLUGIN_ID);
-				if (node == null) {
-					LogUtils.info("Set legacy version : node null !");
+		public ProjectPropertyDescriptor(String propertyName) {
+			this.propertyName = propertyName;
+		}
+
+		private String getValue(IProject project) {
+			IEclipsePreferences node = getProjectPluginNode(project);
+			if (node == null) {
+				LogUtils.info("Get " + propertyName + " : node null !");
+				return null;
+			}
+			String propertyValue = node.get(propertyName, null);
+			if (propertyValue == null || propertyValue.isEmpty()) {
+				return null;
+			}
+
+			return propertyValue;
+		}
+
+		private void setValue(IProject project, String propertyValue) {
+			/*
+			 * On fait l'action dans un job asynchrone pour éviter une ResourceException: The resource tree is locked for modifications
+			 */
+
+			Job job = new Job("KspPreferenceSaving") {
+
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					IEclipsePreferences node = getProjectPluginNode(project);
+					if (node == null) {
+						LogUtils.info("Set " + propertyName + " : node null !");
+						return Status.OK_STATUS;
+					}
+					node.put(propertyName, propertyValue);
+					try {
+						node.flush();
+					} catch (BackingStoreException e) {
+						ErrorUtils.handle(e);
+					}
 					return Status.OK_STATUS;
 				}
-				node.put(LEGACY_VERSION_PROPERTY, legacyVersionName);
-				try {
-					node.flush();
-				} catch (BackingStoreException e) {
-					ErrorUtils.handle(e);
-				}
-				return Status.OK_STATUS;
-			}
-		};
+			};
 
-		job.setPriority(Job.SHORT);
-		job.schedule();
+			job.setPriority(Job.SHORT);
+			job.schedule();
+		}
+
+		private static IEclipsePreferences getProjectPluginNode(IProject project) {
+			IScopeContext projectScope = new ProjectScope(project);
+			return projectScope.getNode(Activator.PLUGIN_ID);
+		}
 	}
 }
